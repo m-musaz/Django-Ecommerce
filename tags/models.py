5038c140a30dc515e5fc001d750470a8032a8408
m-musaz <musazulfqar1@hotmail.com>
2023-05-27 18:17:41 +0500

    Create Tag Models

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+## generic relationship in tagged item
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE) ##generic model using contenttypes app
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+    
+                 
